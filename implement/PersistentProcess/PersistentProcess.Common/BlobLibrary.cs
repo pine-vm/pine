2b54fc610d2df6ab9baf027b0e56d2e40e87c9da
Michael Rätzel <Viir@Viir.de>
2019-10-10 11:02:33 +0000

    Avoid building Elm app failing on dependencies
    + Provide an interface for integrating applications to provide the necessary dependencies. This way, an integrating app can bundle the binaries to load them faster.
    + In case the integrating app does not provide the dependency, increase the timeout for downloading from the internet. Also, enable compression for these downloads.

--- implement/PersistentProcess/PersistentProcess.Common/BlobLibrary.cs
+++ implement/PersistentProcess/PersistentProcess.Common/BlobLibrary.cs
@@ -1,18 +1,33 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
-using System.Security.Cryptography;
+using System.Net.Http.Headers;
 
 namespace Kalmit
 {
     public class BlobLibrary
     {
+        static public Func<Func<byte[], byte[]>, Func<byte[], byte[]>> OverrideGetBlobWithSHA256;
+
         static string cacheDirectory = Path.Combine(Filesystem.CacheDirectory, "blob-library");
 
         static string containerUrl => "https://kalmit.blob.core.windows.net/blob-library";
 
         static public byte[] GetBlobWithSHA256(byte[] sha256)
+        {
+            var getter = OverrideGetBlobWithSHA256?.Invoke(DefaultGetBlobWithSHA256) ?? DefaultGetBlobWithSHA256;
+
+            var blobCandidate = getter(sha256);
+
+            if (!Enumerable.SequenceEqual(CommonConversion.HashSHA256(blobCandidate), sha256))
+                return null;
+
+            return blobCandidate;
+        }
+
+        static public byte[] DefaultGetBlobWithSHA256(byte[] sha256)
         {
             var sha256DirectoryName = "by-sha256";
 
@@ -20,8 +35,8 @@ namespace Kalmit
 
             var cacheFilePath = Path.Combine(cacheDirectory, sha256DirectoryName, fileName);
 
-            bool blobHasExpectedSHA256(byte[] blob) =>
-                Enumerable.SequenceEqual(CommonConversion.HashSHA256(blob), sha256);
+            bool blobHasExpectedSHA256(byte[] blobCandidate) =>
+                Enumerable.SequenceEqual(CommonConversion.HashSHA256(blobCandidate), sha256);
 
             try
             {
@@ -35,20 +50,30 @@ namespace Kalmit
 
             var url = containerUrl + "/" + sha256DirectoryName + "/" + fileName;
 
-            var httpClient = new HttpClient();
+            var handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip
+            };
 
-            var response = httpClient.GetAsync(url).Result;
+            using (var httpClient = new HttpClient(handler))
+            {
+                httpClient.Timeout = TimeSpan.FromMinutes(4);
+                httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+                httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
 
-            var responseContent = response.Content.ReadAsByteArrayAsync().Result;
+                var response = httpClient.GetAsync(url).Result;
 
-            if (!blobHasExpectedSHA256(responseContent))
-                throw new NotImplementedException("Received unexpected blob.");
+                var responseContent = response.Content.ReadAsByteArrayAsync().Result;
 
-            Directory.CreateDirectory(Path.GetDirectoryName(cacheFilePath));
+                if (!blobHasExpectedSHA256(responseContent))
+                    throw new NotImplementedException("Received unexpected blob.");
 
-            File.WriteAllBytes(cacheFilePath, responseContent);
+                Directory.CreateDirectory(Path.GetDirectoryName(cacheFilePath));
 
-            return responseContent;
+                File.WriteAllBytes(cacheFilePath, responseContent);
+
+                return responseContent;
+            }
         }
     }
 }
