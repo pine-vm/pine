9b2d3c2d625f14a4e33fbeab36e98066a1fff2ef
Michael Rätzel <viir@viir.de>
2020-02-12 14:51:36 +0000

    Refine user interface for running server
    + Support configuring the server using process arguments.
    + Support common use-cases: Add an option to delete the previous state of the backend when starting a server.

--- implement/elm-fullstack/Program.cs
+++ implement/elm-fullstack/Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
+using Kalmit.PersistentProcess.WebHost;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace elm_fullstack
 {
     class Program
     {
-        static string AppVersionId => "2020-02-11";
+        static string AppVersionId => "2020-02-12";
 
         static int Main(string[] args)
         {
             var app = new CommandLineApplication
             {
                 Name = "elm-fullstack",
-                Description = "Welcome to Elm fullstack! This tool helps you build and run fullstack web applications using the Elm programming language.\nTo get help or report an issue, see the project website at http://elm-fullstack.org/",
+                Description = "Welcome to Elm fullstack! This tool helps you build and run full stack web applications using the Elm programming language.\nTo get help or report an issue, see the project website at http://elm-fullstack.org/",
             };
 
             app.HelpOption(inherited: true);
@@ -25,9 +26,29 @@ namespace elm_fullstack
 
                 runServerCmd.ThrowOnUnexpectedArgument = false;
 
+                var processStoreDirectoryPathOption = runServerCmd.Option("--process-store-directory-path", "Directory in the file system to contain the backend process store.", CommandOptionType.SingleValue).IsRequired(allowEmptyStrings: false);
+                var webAppConfigurationFilePathOption = runServerCmd.Option("--web-app-configuration-file-path", "Path to a file containing the complete configuration in a zip-archive. If you don't use this option, the server uses the current directory as the source.", CommandOptionType.SingleValue);
+                var deletePreviousBackendStateOption = runServerCmd.Option("--delete-previous-backend-state", "Delete the previous state of the backend process. If you don't use this option, the server restores the last state backend on startup.", CommandOptionType.NoValue);
+
                 runServerCmd.OnExecute(() =>
                 {
+                    var processStoreDirectoryPath = processStoreDirectoryPathOption.Value();
+
+                    if (deletePreviousBackendStateOption.HasValue())
+                    {
+                        Console.WriteLine("Deleting the previous backend state from '" + processStoreDirectoryPath + "'");
+                        System.IO.Directory.Delete(processStoreDirectoryPath, true);
+                    }
+
                     var webHostBuilder = Kalmit.PersistentProcess.WebHost.Program.CreateWebHostBuilder(runServerCmd.RemainingArguments.ToArray());
+
+                    webHostBuilder.WithSettingProcessStoreDirectoryPath(processStoreDirectoryPath);
+
+                    if (webAppConfigurationFilePathOption.HasValue())
+                    {
+                        webHostBuilder.WithSettingWebAppConfigurationFilePath(webAppConfigurationFilePathOption.Value());
+                    }
+
                     Microsoft.AspNetCore.Hosting.WebHostExtensions.Run(webHostBuilder.Build());
                 });
             });
