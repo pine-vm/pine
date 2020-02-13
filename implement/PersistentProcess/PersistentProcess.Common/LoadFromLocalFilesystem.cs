e4c5fd8b27e6034a082ab79d4f7747dcf17b994c
Michael Rätzel <viir@viir.de>
2020-02-13 13:13:46 +0000

    Implementation to load composition from files

---
+++ implement/PersistentProcess/PersistentProcess.Common/LoadFromLocalFilesystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Kalmit
+{
+    static public class LoadFromLocalFilesystem
+    {
+        static public Composition.TreeComponent LoadTreeFromPath(string path)
+        {
+            if (File.Exists(path))
+                return new Composition.TreeComponent { BlobContent = File.ReadAllBytes(path).ToImmutableList() };
+
+            if (!Directory.Exists(path))
+                return null;
+
+            var treeEntries =
+                Directory.EnumerateFileSystemEntries(path)
+                .Select(fileSystemEntry =>
+                {
+                    var name = (IImmutableList<byte>)Encoding.BigEndianUnicode.GetBytes(Path.GetRelativePath(path, fileSystemEntry)).ToImmutableList();
+
+                    return (name, LoadTreeFromPath(fileSystemEntry));
+                })
+                .ToImmutableList();
+
+            return new Composition.TreeComponent
+            {
+                TreeContent = treeEntries,
+            };
+        }
+    }
+}
