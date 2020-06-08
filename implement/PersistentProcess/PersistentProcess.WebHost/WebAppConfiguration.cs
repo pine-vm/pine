b5273ede7cd6c876f7430a92a329d9e0b7e6714a
Michael Rätzel <viir@viir.de>
2020-06-08 09:19:52 +0000

    Remove obsolete code

--- implement/PersistentProcess/PersistentProcess.WebHost/WebAppConfiguration.cs
+++ implement/PersistentProcess/PersistentProcess.WebHost/WebAppConfiguration.cs
@@ -2,13 +2,9 @@ namespace Kalmit.PersistentProcess
 {
     public class WebAppConfigurationJsonStructure
     {
-        public ConditionalMapFromStringToString[] mapsFromRequestUrlToStaticFileName;
-
         public RateLimitWindow singleRateLimitWindowPerClientIPv4Address;
 
         public FluffySpoon.AspNet.LetsEncrypt.LetsEncryptOptions letsEncryptOptions;
-
-        public string frontendWebElmMakeCommandAppendix;
 
         public class ConditionalMapFromStringToString
         {
