c66a7ec746fa6eb4b769f2914c53f3c7b3fb4a33
Michael Rätzel <viir@viir.de>
2021-01-16 20:51:26 +0000

    Integrate dotnet security patches

--- implement/PersistentProcess/PersistentProcess.WebHost/Program.cs
+++ implement/PersistentProcess/PersistentProcess.WebHost/Program.cs
@@ -2,6 +2,6 @@ namespace Kalmit.PersistentProcess.WebHost
 {
     public class Program
     {
-        static public string AppVersionId => "2021-01-09";
+        static public string AppVersionId => "2021-01-16";
     }
 }
