ba4a18c7d460400f9a37b5325be6e8cc209d6757
Michael Rätzel <viir@viir.de>
2021-03-09 17:02:35 +0000

    Offer to adapt screen space division to enlarge editor or output
    Add a toggle button on the editor and the output pane to enlarge it or revert to the default size.

--- implement/example-apps/elm-editor/src/FrontendWeb/Main.elm
+++ implement/example-apps/elm-editor/src/FrontendWeb/Main.elm
@@ -82,7 +82,13 @@ type alias WorkingProjectStateStructure =
     , pendingElmMakeRequest : Maybe { time : Time.Posix, request : ElmMakeRequestStructure }
     , elmMakeResult : Maybe ( ElmMakeRequestStructure, Result Http.Error ElmMakeResultStructure )
     , elmFormatResult : Maybe (Result Http.Error FrontendBackendInterface.FormatElmModuleTextResponseStructure)
+    , viewEnlargedPane : Maybe WorkspacePane
     }
+
+
+type WorkspacePane
+    = EditorPane
+    | OutputPane
 
 
 type alias ElmMakeResultStructure =
@@ -113,6 +119,7 @@ type WorkspaceEventStructure
     | UserInputRevealPositionInEditor { filePath : List String, lineNumber : Int, column : Int }
     | BackendElmFormatResponseEvent { filePath : List String, result : Result Http.Error FrontendBackendInterface.FormatElmModuleTextResponseStructure }
     | BackendElmMakeResponseEvent ElmMakeRequestStructure (Result Http.Error ElmMakeResponseStructure)
+    | UserInputSetEnlargedPane (Maybe WorkspacePane)
 
 
 type UserInputLoadFromGitEventStructure
@@ -511,13 +518,13 @@ updateWorkspaceWithoutCmdToUpdateEditor updateConfig event stateBefore =
                             updateWorkspaceWithoutCmdToUpdateEditor updateConfig UserInputFormat stateBefore
 
                         FrontendWeb.MonacoEditor.EditorActionCompileEvent ->
-                            userInputCompileFileOpenedInEditor updateConfig stateBefore
+                            updateWorkspaceWithoutCmdToUpdateEditor updateConfig UserInputCompile stateBefore
 
         UserInputFormat ->
             ( stateBefore, elmFormatCmd stateBefore |> Maybe.withDefault Cmd.none )
 
         UserInputCompile ->
-            userInputCompileFileOpenedInEditor updateConfig stateBefore
+            userInputCompileFileOpenedInEditor updateConfig { stateBefore | viewEnlargedPane = Nothing }
 
         BackendElmFormatResponseEvent formatResponseEvent ->
             ( if Just formatResponseEvent.filePath /= stateBefore.editing.filePathOpenedInEditor then
@@ -585,6 +592,9 @@ updateWorkspaceWithoutCmdToUpdateEditor updateConfig event stateBefore =
               }
             , Cmd.none
             )
+
+        UserInputSetEnlargedPane enlargedPane ->
+            ( { stateBefore | viewEnlargedPane = enlargedPane }, Cmd.none )
 
 
 processEventUrlChanged : Url.Url -> State -> ( State, Cmd Event )
@@ -982,6 +992,35 @@ view state =
 
                 WorkspaceOk workingState ->
                     let
+                        paneLayout paneProperties =
+                            let
+                                widthFillPortion =
+                                    if workingState.viewEnlargedPane == Just paneProperties.pane then
+                                        40
+
+                                    else
+                                        4
+                            in
+                            [ [ paneProperties.buttons
+                                    |> Element.row
+                                        [ Element.spacing defaultFontSize
+                                        , Element.width Element.fill
+                                        , Element.clipX
+                                        ]
+                              , toggleEnlargedPaneButton workingState paneProperties.pane |> Element.map WorkspaceEvent
+                              ]
+                                |> Element.row
+                                    [ Element.spacing defaultFontSize
+                                    , Element.padding (defaultFontSize // 2)
+                                    , Element.width Element.fill
+                                    ]
+                            , paneProperties.mainContent
+                            ]
+                                |> Element.column
+                                    [ Element.width (Element.fillPortion widthFillPortion |> Element.minimum 80)
+                                    , Element.height Element.fill
+                                    ]
+
                         workspaceView =
                             case workingState.editing.filePathOpenedInEditor of
                                 Nothing ->
@@ -1057,35 +1096,27 @@ view state =
                                         monacoEditorElement state
                                     }
                     in
-                    [ [ workspaceView.editorPaneButtons
-                            |> Element.row
-                                [ Element.spacing defaultFontSize
-                                , Element.padding (defaultFontSize // 2)
-                                ]
-                      , workspaceView.editorPaneContent
-                      ]
-                        |> Element.column
-                            [ Element.spacing (defaultFontSize // 2)
-                            , Element.width (Element.fillPortion 4)
-                            , Element.height Element.fill
-                            ]
-                    , [ workspaceView.outputPaneButtons |> Element.row [ Element.padding (defaultFontSize // 2) ]
-                      , workingState
-                            |> viewOutputPaneContent
-                            |> Element.el
-                                [ Element.width Element.fill
-                                , Element.height Element.fill
+                    [ paneLayout
+                        { pane = EditorPane
+                        , buttons = workspaceView.editorPaneButtons
+                        , mainContent = workspaceView.editorPaneContent
+                        }
+                    , paneLayout
+                        { pane = OutputPane
+                        , buttons = workspaceView.outputPaneButtons
+                        , mainContent =
+                            workingState
+                                |> viewOutputPaneContent
+                                |> Element.el
+                                    [ Element.width Element.fill
+                                    , Element.height Element.fill
 
-                                -- https://github.com/mdgriffith/elm-ui/issues/149#issuecomment-531480958
-                                , Element.clip
-                                , Element.htmlAttribute (HA.style "flex-shrink" "1")
-                                ]
-                            |> Element.map WorkspaceEvent
-                      ]
-                        |> Element.column
-                            [ Element.width (Element.fillPortion 4)
-                            , Element.height Element.fill
-                            ]
+                                    -- https://github.com/mdgriffith/elm-ui/issues/149#issuecomment-531480958
+                                    , Element.clip
+                                    , Element.htmlAttribute (HA.style "flex-shrink" "1")
+                                    ]
+                                |> Element.map WorkspaceEvent
+                        }
                     ]
                         |> Element.row [ Element.width Element.fill, Element.height Element.fill ]
 
@@ -1214,6 +1245,35 @@ view state =
                     )
     in
     { title = "Elm Editor", body = [ body ] }
+
+
+toggleEnlargedPaneButton : WorkingProjectStateStructure -> WorkspacePane -> Element.Element WorkspaceEventStructure
+toggleEnlargedPaneButton state pane =
+    let
+        isPaneEnlarged =
+            state.viewEnlargedPane == Just pane
+
+        ( icon, onPress ) =
+            if isPaneEnlarged then
+                ( ShrinkActionIcon, Nothing )
+
+            else
+                ( GrowActionIcon, Just pane )
+
+        iconSize =
+            24
+    in
+    Element.Input.button
+        [ Element.Background.color (Element.rgb 0.2 0.2 0.2)
+        , Element.mouseOver [ Element.Background.color (Element.rgb 0 0.5 0.8) ]
+        , Element.padding 4
+        ]
+        { label =
+            iconSvgElementFromIcon { color = "#aaa", viewBoxWidth = 24, viewBoxHeight = 24 } icon
+                |> Element.el [ Element.width (Element.px iconSize), Element.height (Element.px iconSize) ]
+        , onPress = Just (UserInputSetEnlargedPane onPress)
+        }
+        |> Element.el [ Element.alignRight ]
 
 
 type alias PopupAttributes event =
@@ -1385,10 +1445,17 @@ activityBar =
 type ActionIcon
     = ChatActionIcon
     | GitHubActionIcon
+    | GrowActionIcon
+    | ShrinkActionIcon
 
 
 actionIconSvgElementFromIcon : ActionIcon -> Element.Element event
-actionIconSvgElementFromIcon iconType =
+actionIconSvgElementFromIcon =
+    iconSvgElementFromIcon { color = "white", viewBoxWidth = 24, viewBoxHeight = 24 }
+
+
+iconSvgElementFromIcon : { color : String, viewBoxWidth : Int, viewBoxHeight : Int } -> ActionIcon -> Element.Element event
+iconSvgElementFromIcon { color, viewBoxWidth, viewBoxHeight } iconType =
     let
         pathsElements =
             actionIconSvgPathsData iconType
@@ -1406,8 +1473,8 @@ actionIconSvgElementFromIcon iconType =
                     )
     in
     Svg.svg
-        [ Svg.Attributes.viewBox ([ 0, 0, 24, 24 ] |> List.map String.fromInt |> String.join " ")
-        , Svg.Attributes.fill "white"
+        [ Svg.Attributes.viewBox ([ 0, 0, viewBoxWidth, viewBoxHeight ] |> List.map String.fromInt |> String.join " ")
+        , Svg.Attributes.fill color
         ]
         pathsElements
         |> Element.html
@@ -1426,6 +1493,24 @@ actionIconSvgPathsData icon =
         GitHubActionIcon ->
             -- https://github.com/microsoft/vscode-codicons/tree/e1155a851abafe070be17d36996474f4f374741f/src/icons
             [ { pathData = "M12 1a11 11 0 1 0 0 22 11 11 0 0 0 0-22zm2.9 19.968h-.086a.471.471 0 0 1-.35-.129.471.471 0 0 1-.129-.34v-1.29c.006-.428.01-.86.01-1.297a3.385 3.385 0 0 0-.139-.943 1.679 1.679 0 0 0-.496-.802 7.34 7.34 0 0 0 1.868-.432 3.715 3.715 0 0 0 1.344-.883c.373-.392.65-.864.81-1.381.196-.632.289-1.29.276-1.952a3.797 3.797 0 0 0-.24-1.353 3.569 3.569 0 0 0-.727-1.177c.068-.172.118-.351.148-.534a3.286 3.286 0 0 0-.036-1.262 4.87 4.87 0 0 0-.203-.7.269.269 0 0 0-.102-.018h-.1c-.21.002-.419.037-.618.102-.22.064-.436.144-.645.239a5.97 5.97 0 0 0-.606.314 9.992 9.992 0 0 0-.525.332 8.78 8.78 0 0 0-4.714 0 12.367 12.367 0 0 0-.525-.332 5.52 5.52 0 0 0-.616-.314 4.14 4.14 0 0 0-.646-.239 2.02 2.02 0 0 0-.607-.102h-.1a.266.266 0 0 0-.1.019 5.356 5.356 0 0 0-.213.699 3.441 3.441 0 0 0-.027 1.262c.03.183.079.362.147.534a3.565 3.565 0 0 0-.726 1.177 3.797 3.797 0 0 0-.24 1.353 6.298 6.298 0 0 0 .266 1.942c.167.517.443.992.811 1.391.38.386.838.687 1.344.883.598.23 1.225.377 1.863.437-.178.161-.32.36-.414.58-.09.219-.153.448-.184.682a2.524 2.524 0 0 1-1.077.248 1.639 1.639 0 0 1-.976-.276 2.661 2.661 0 0 1-.69-.755 2.914 2.914 0 0 0-.267-.35 2.459 2.459 0 0 0-.34-.314 1.687 1.687 0 0 0-.397-.22 1.1 1.1 0 0 0-.441-.093.942.942 0 0 0-.11.01c-.05 0-.1.006-.148.018a.376.376 0 0 0-.12.055.107.107 0 0 0-.054.091.304.304 0 0 0 .129.222c.084.068.155.12.212.157l.026.019c.123.094.24.196.35.305.104.09.197.192.276.303.083.108.154.226.212.349.067.123.138.264.212.424.172.434.478.802.874 1.05.415.223.882.334 1.353.322.16 0 .32-.01.48-.028.156-.025.313-.052.47-.083v1.598a.459.459 0 0 1-.488.477h-.057a9.428 9.428 0 1 1 5.797 0v.005z"
+              , fillNone = False
+              }
+            ]
+
+        GrowActionIcon ->
+            [ { pathData = "M14.5 0L14.5 2.5L19.5 2.5L13.5 8.5L15.5 10.5L21.0 5.0L21.0 9.5L24.0 9.5L24.0 0L14.5 0Z"
+              , fillNone = False
+              }
+            , { pathData = "M9.5 24.0L9.5 21.5L4.5 21.5L10.5 15.5L8.5 13.5L3.0 19.0L3.0 14.5L0 14.5L0 24.0L9.5 24.0Z"
+              , fillNone = False
+              }
+            ]
+
+        ShrinkActionIcon ->
+            [ { pathData = "M23.0 10.5L23.0 8.0L18.0 8.0L24.0 2.0L22.0 0L16.5 5.5L16.5 1.0L13.5 1.0L13.5 10.5L23.0 10.5Z"
+              , fillNone = False
+              }
+            , { pathData = "M1.0 13.5L1.0 16.0L6.0 16.0L0 22.0L2.0 24.0L7.5 18.5L7.5 23.0L10.5 23.0L10.5 13.5L1.0 13.5Z"
               , fillNone = False
               }
             ]
@@ -1902,6 +1987,7 @@ initWorkspaceFromFileTreeAndFileSelection { fileTree, filePathOpenedInEditor } =
     , pendingElmMakeRequest = Nothing
     , elmMakeResult = Nothing
     , elmFormatResult = Nothing
+    , viewEnlargedPane = Nothing
     }
 
 
