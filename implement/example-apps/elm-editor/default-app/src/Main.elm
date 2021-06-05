8af79f5345d9b87dcdd2d1d52f7f2516f7efe8af
Michael Rätzel <viir@viir.de>
2021-06-05 16:19:21 +0000

    Use a default app that also demonstrates interactivity

--- implement/example-apps/elm-editor/default-app/src/Main.elm
+++ implement/example-apps/elm-editor/default-app/src/Main.elm
@@ -1,9 +1,52 @@
 module Main exposing (..)
 
+-- Press buttons to increment and decrement a counter.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/buttons.html
+--
+
+import Browser
 import Html
 import Html.Attributes
+import Html.Events
 
 
 main =
-    [ Html.text "Hello World!" ]
-        |> Html.div [ Html.Attributes.style "color" "whitesmoke" ]
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias State =
+    Int
+
+
+init : State
+init =
+    0
+
+
+type Event
+    = Increment
+    | Decrement
+
+
+update : Event -> State -> State
+update event state =
+    case event of
+        Increment ->
+            state + 1
+
+        Decrement ->
+            state - 1
+
+
+view : State -> Html.Html Event
+view state =
+    Html.div
+        [ Html.Attributes.style "background" "#222"
+        , Html.Attributes.style "color" "whitesmoke"
+        ]
+        [ Html.button [ Html.Events.onClick Decrement ] [ Html.text "-" ]
+        , Html.div [] [ Html.text (String.fromInt state) ]
+        , Html.button [ Html.Events.onClick Increment ] [ Html.text "+" ]
+        ]
