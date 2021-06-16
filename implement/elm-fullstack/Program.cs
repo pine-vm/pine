6a89ca6cd8ab30dbda7e93cd01de3b813e33ee2a
Michael Rätzel <viir@viir.de>
2021-06-16 08:41:34 +0000

    Fix dependency on `ICU package` discovered on Ubuntu 20 🐛🧹
    Fix the `ICU package` error discovered in https://github.com/elm-fullstack/elm-fullstack/commit/c2f034b216e3a73bb8f5fcb9820bd5237055e40c
    
    Work around the issue described at https://github.com/dotnet/core/issues/2186 and https://github.com/dotnet/core/issues/5019
    
    Integrate the fix shared by @petrsvihlik at https://github.com/dotnet/core/issues/2186#issuecomment-726708332

--- implement/elm-fullstack/Program.cs
+++ implement/elm-fullstack/Program.cs
@@ -14,7 +14,7 @@ namespace elm_fullstack
 {
     public class Program
     {
-        static public string AppVersionId => "2021-06-05";
+        static public string AppVersionId => "2021-06-16";
 
         static int AdminInterfaceDefaultPort => 4000;
 
