f6bccc95add7755ccae31ac310b6cdd99c2ef7d7
Michael Rätzel <viir@viir.de>
2021-11-08 20:18:15 +0000

    Switch to a longer supported version of .NET

--- implement/elm-fullstack/Program.cs
+++ implement/elm-fullstack/Program.cs
@@ -14,7 +14,7 @@ namespace elm_fullstack
 {
     public class Program
     {
-        static public string AppVersionId => "2021-11-02";
+        static public string AppVersionId => "2021-11-08";
 
         static int AdminInterfaceDefaultPort => 4000;
 
