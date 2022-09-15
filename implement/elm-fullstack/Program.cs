043fac1e59f756d98366d72ae228a93a083578f6
Michael Rätzel <viir@viir.de>
2022-09-15 06:41:30 +0000

    Update to .NET 7 RC1
    https://devblogs.microsoft.com/dotnet/announcing-dotnet-7-rc-1/

--- implement/elm-fullstack/Program.cs
+++ implement/elm-fullstack/Program.cs
@@ -15,7 +15,7 @@ namespace ElmFullstack;
 
 public class Program
 {
-    static public string AppVersionId => "2022-09-13";
+    static public string AppVersionId => "2022-09-15";
 
     static int AdminInterfaceDefaultPort => 4000;
 
