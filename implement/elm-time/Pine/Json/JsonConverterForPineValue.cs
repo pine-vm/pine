a76f091bb3be9b399dc346d50ccb0fbaf307faf6
Michael Rätzel <viir@viir.de>
2023-02-20 14:06:42 +0000

    Support inspection and transport with JSON converter for PineValue
    Support inspection with a representation optimized for improved readability.

---
+++ implement/elm-time/Pine/Json/JsonConverterForPineValue.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Pine.Json;
+
+public class JsonConverterForPineValue : JsonConverter<PineValue>
+{
+    public override PineValue? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Number)
+        {
+            var integerString = System.Text.Encoding.UTF8.GetString(reader.ValueSpan);
+
+            if (System.Numerics.BigInteger.TryParse(integerString, out var integer))
+            {
+                reader.Read();
+
+                return Composition.ComponentFromSignedInteger(integer);
+            }
+        }
+
+        if (reader.TokenType == JsonTokenType.StartArray)
+        {
+            reader.Read();
+
+            var elements = new List<PineValue>();
+
+            while (reader.TokenType != JsonTokenType.EndArray)
+            {
+                elements.Add(Read(ref reader, typeToConvert: typeToConvert, options: options)!);
+            }
+
+            reader.Read();
+
+            return PineValue.List(elements);
+        }
+
+        if (reader.TokenType == JsonTokenType.StartObject)
+        {
+            reader.Read();
+
+            if (reader.TokenType is not JsonTokenType.PropertyName)
+            {
+                throw new NotSupportedException("Unexpected token type in object: " + reader.TokenType);
+            }
+
+            var propertyName = reader.GetString();
+
+            reader.Read();
+
+            if (propertyName is "AsBase64")
+            {
+                var pineValue = PineValue.Blob(reader.GetBytesFromBase64());
+
+                reader.Read();
+
+                if (reader.TokenType is not JsonTokenType.EndObject)
+                    throw new NotSupportedException("Unexpected token type in object: " + reader.TokenType);
+
+                return pineValue;
+            }
+
+
+            if (propertyName is "ListAsString")
+            {
+                var pineValue = Composition.ComponentFromString(reader.GetString());
+
+                reader.Read();
+
+                if (reader.TokenType is not JsonTokenType.EndObject)
+                    throw new NotSupportedException("Unexpected token type in object: " + reader.TokenType);
+
+                return pineValue;
+            }
+
+            throw new NotSupportedException("Unsupported property name: " + propertyName);
+        }
+
+        throw new NotImplementedException("Unexpected token type: " + reader.TokenType);
+    }
+
+    public override void Write(Utf8JsonWriter writer, PineValue value, JsonSerializerOptions options)
+    {
+        if (Composition.SignedIntegerFromComponent(value) is Result<string, System.Numerics.BigInteger>.Ok asInt)
+        {
+            if (asInt.Value < long.MaxValue && long.MinValue < asInt.Value)
+            {
+                writer.WriteNumberValue((long)asInt.Value);
+
+                return;
+            }
+        }
+
+        if (Composition.StringFromComponent(value) is Result<string, string>.Ok asString && 0 < asString.Value.Length)
+        {
+            if (!asString.Value.All(asChar => ((asChar & 0xff00) == 0x400) || ((asChar & 0xff00) == 0x200)))
+            {
+                writer.WriteStartObject();
+
+                writer.WriteString("ListAsString", asString.Value);
+
+                writer.WriteEndObject();
+
+                return;
+            }
+        }
+
+        WriteDefaultRepresentation(writer, value, options);
+    }
+
+    void WriteDefaultRepresentation(Utf8JsonWriter writer, PineValue value, JsonSerializerOptions options)
+    {
+        if (value is PineValue.ListValue list)
+        {
+            writer.WriteStartArray();
+
+            foreach (var item in list.Elements)
+            {
+                Write(writer, item, options);
+            }
+
+            writer.WriteEndArray();
+
+            return;
+        }
+
+        if (value is PineValue.BlobValue blob)
+        {
+            writer.WriteStartObject();
+
+            writer.WriteBase64String("AsBase64", blob.Bytes.Span);
+
+            writer.WriteEndObject();
+
+            return;
+        }
+
+        throw new NotImplementedException();
+    }
+}
---
+++ implement/test-elm-time/TestJsonConverterForPineValue.cs
@@ -0,0 +1,95 @@
+using Microsoft.Extensions.Primitives;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pine;
+using Pine.Json;
+using System.Collections.Immutable;
+using System.Text.Json;
+
+namespace TestElmTime;
+
+[TestClass]
+public class TestJsonConverterForPineValue
+{
+    [TestMethod]
+    public void JSON_serialize_pine_value_roundtrips()
+    {
+        var jsonSerializerOptions = new JsonSerializerOptions { };
+
+        jsonSerializerOptions.Converters.Add(new JsonConverterForPineValue());
+
+        var testCases = new[]
+        {
+            Composition.ComponentFromSignedInteger(0),
+            Composition.ComponentFromSignedInteger(1234),
+            Composition.ComponentFromSignedInteger(-45678),
+
+            PineValue.EmptyList,
+
+            PineValue.List(
+                ImmutableArray.Create(
+                    Composition.ComponentFromSignedInteger(56),
+                    Composition.ComponentFromSignedInteger(57))),
+
+            PineValue.Blob(new byte[]{ }),
+            PineValue.Blob(new byte[]{10,11,13}),
+        };
+
+        foreach (var testCase in testCases)
+        {
+            var asJson = JsonSerializer.Serialize(
+                testCase,
+                options: jsonSerializerOptions);
+
+            var fromJson = JsonSerializer.Deserialize<PineValue>(
+                asJson,
+                options: jsonSerializerOptions);
+
+            Assert.AreEqual(testCase, fromJson);
+        }
+    }
+
+    [TestMethod]
+    public void JSON_serialize_pine_value_to_native_integer()
+    {
+        var jsonSerializerOptions = new JsonSerializerOptions { };
+
+        jsonSerializerOptions.Converters.Add(new JsonConverterForPineValue());
+
+        string serializePineValue(PineValue pineValue) =>
+            JsonSerializer.Serialize(
+                pineValue,
+                options: jsonSerializerOptions);
+
+        Assert.AreEqual(
+            "1234",
+            serializePineValue(
+                Composition.ComponentFromSignedInteger(1234)));
+
+        Assert.AreEqual(
+            "-34567",
+            serializePineValue(
+                Composition.ComponentFromSignedInteger(-34567)));
+    }
+
+    [TestMethod]
+    public void JSON_serialize_pine_value_to_native_string()
+    {
+        var jsonSerializerOptions = new JsonSerializerOptions { };
+
+        jsonSerializerOptions.Converters.Add(new JsonConverterForPineValue());
+
+        string serializePineValue(PineValue pineValue) =>
+            JsonSerializer.Serialize(
+                pineValue,
+                options: jsonSerializerOptions);
+
+        Assert.AreEqual(
+            $$"""{"ListAsString":"stringValue 789"}""",
+            serializePineValue(
+                Composition.ComponentFromString("stringValue 789")));
+
+        Assert.AreEqual(
+            "[]",
+            serializePineValue(PineValue.EmptyList));
+    }
+}
