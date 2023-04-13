028e69b766ce82beec8aafc826be9aadf131b08d
Michael Rätzel <viir@viir.de>
2023-04-13 06:58:05 +0000

    Add patch for LibGit2Sharp to fix cloning from git
    See https://github.com/libgit2/libgit2sharp/issues/2021#issuecomment-1506366633

--- implement/elm-time/Program.cs
+++ implement/elm-time/Program.cs
@@ -18,7 +18,7 @@ namespace ElmTime;
 
 public class Program
 {
-    public static string AppVersionId => "2023-04-12";
+    public static string AppVersionId => "2023-04-13";
 
     private static int AdminInterfaceDefaultPort => 4000;
 
