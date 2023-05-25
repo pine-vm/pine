a70ccd27ea96ef263b5e8a466e16aca64945b03e
Michael Rätzel <viir@viir.de>
2023-05-25 17:46:46 +0000

    Update website to release version 2023-05-25

--- implement/website/elm-time/src/Frontend/Page/Download.elm
+++ implement/website/elm-time/src/Frontend/Page/Download.elm
@@ -48,9 +48,9 @@ dockerImageUrl =
 
 downloads : DownloadLinksByPlatform
 downloads =
-    { linux = "https://github.com/elm-time/elm-time/releases/download/v2023-05-21/elm-time-bin-v2023-05-21-linux-x64.zip"
-    , windows = "https://github.com/elm-time/elm-time/releases/download/v2023-05-21/elm-time-bin-v2023-05-21-win10-x64.zip"
-    , macOS = "https://github.com/elm-time/elm-time/releases/download/v2023-05-21/elm-time-bin-v2023-05-21-osx-x64.zip"
+    { linux = "https://github.com/elm-time/elm-time/releases/download/v2023-05-25/elm-time-bin-v2023-05-25-linux-x64.zip"
+    , windows = "https://github.com/elm-time/elm-time/releases/download/v2023-05-25/elm-time-bin-v2023-05-25-win10-x64.zip"
+    , macOS = "https://github.com/elm-time/elm-time/releases/download/v2023-05-25/elm-time-bin-v2023-05-25-osx-x64.zip"
     }
 
 
