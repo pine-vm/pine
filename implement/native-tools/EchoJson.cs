74c0bbc4fb8ea4e3511a080e4e0fd5541793ef2c
Michael Rätzel <viir@viir.de>
2023-06-30 20:24:51 +0000

    Begin native tools CLI as example program for native volatile process
    This project builds an executable file for each supported OS that we can use with the planned new (native) volatile process model.

---
+++ implement/native-tools/EchoJson.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json;
+
+namespace native_tools;
+
+public class EchoJson
+{
+    public static int EchoJsonLoop()
+    {
+        while (true)
+        {
+            var lineText = Console.ReadLine()?.Trim();
+
+            if (string.IsNullOrEmpty(lineText))
+            {
+                continue;
+            }
+
+            try
+            {
+                var json = JsonSerializer.Deserialize<JsonElement>(lineText);
+                Console.WriteLine(JsonSerializer.Serialize(json) + "\n");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Failed with runtime exception:\n" + ex);
+                return 1;
+            }
+        }
+    }
+}
---
+++ implement/native-tools/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.CommandLine;
+using System.Threading.Tasks;
+
+namespace native_tools;
+
+public class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var rootCommand = new RootCommand("Collection of native tools");
+
+        var echoJsonCommand = new Command("echo-json", "Echoes the JSON string to the console.");
+
+        echoJsonCommand.SetHandler(() =>
+        {
+            EchoJson.EchoJsonLoop();
+        });
+
+        rootCommand.Add(echoJsonCommand);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+}
