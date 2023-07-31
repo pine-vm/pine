02ef9f5c72628dd35d52806bb2035e62285b2ff5
Michael Rätzel <viir@viir.de>
2023-07-31 21:09:29 +0000

    Integrate the many recent improvements in Jint
    For an overview of the recent changes, see https://github.com/sebastienros/jint/releases/tag/v3.0.0-beta-2050

--- implement/elm-time/Program.cs
+++ implement/elm-time/Program.cs
@@ -17,7 +17,7 @@ namespace ElmTime;
 
 public class Program
 {
-    public static string AppVersionId => "2023-07-30";
+    public static string AppVersionId => "2023-07-31";
 
     private static int AdminInterfaceDefaultPort => 4000;
 
