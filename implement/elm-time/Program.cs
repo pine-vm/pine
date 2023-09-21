9b06869faf618fbe2cb2b155cc1c2b991cf7d655
Michael Rätzel <viir@viir.de>
2023-09-21 17:08:26 +0000

    Fix precedence for operators from core module Basics

--- implement/elm-time/Program.cs
+++ implement/elm-time/Program.cs
@@ -18,7 +18,7 @@ namespace ElmTime;
 
 public class Program
 {
-    public static string AppVersionId => "2023-09-18";
+    public static string AppVersionId => "2023-09-19";
 
     private static int AdminInterfaceDefaultPort => 4000;
 
