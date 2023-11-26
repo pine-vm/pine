7b8ab210f8da1ee02b1a6e6abd78eb3c23bb657c
Michael Rätzel <viir@viir.de>
2023-11-26 20:25:11 +0000

    Apply patches recommended from upstream projects

--- implement/elm-time/Program.cs
+++ implement/elm-time/Program.cs
@@ -18,7 +18,7 @@ namespace ElmTime;
 
 public class Program
 {
-    public static string AppVersionId => "2023-11-24";
+    public static string AppVersionId => "2023-11-26";
 
     private static int AdminInterfaceDefaultPort => 4000;
 
