1c7d3e47f6b847b5302eed07d27c4b3e624f15b8
Michael Rätzel <viir@viir.de>
2024-02-16 07:51:28 +0000

    Apply recommended patches of upstream projects
    Skipping LibGit2Sharp, because switching to version 0.29 resulted in crashes like these:
    
    Windows:
    ----
    Unhandled exception. System.Exception: Failed to clone from 'https://github.com/elm-time/elm-time.git'
     ---> System.TypeInitializationException: The type initializer for 'LibGit2Sharp.Core.NativeMethods' threw an exception.
     ---> System.DllNotFoundException: Dll was not found.
       at LibGit2Sharp.Core.NativeMethods.git_libgit2_init()
       at LibGit2Sharp.Core.NativeMethods.InitializeNativeLibrary() in /_/LibGit2Sharp/Core/NativeMethods.cs:line 148
       at LibGit2Sharp.Core.NativeMethods..cctor() in /_/LibGit2Sharp/Core/NativeMethods.cs:line 54
       --- End of inner exception stack trace ---
    
    macOS:
    ----
    Unhandled exception. System.Exception: Failed to clone from 'https://github.com/elm-time/elm-time.git'
     ---> System.TypeInitializationException: The type initializer for 'LibGit2Sharp.Core.NativeMethods' threw an exception.
     ---> System.DllNotFoundException: Unable to load shared library 'git2-a2bde63' or one of its dependencies. In order to help diagnose loading problems, consider setting the DYLD_PRINT_LIBRARIES environment variable:

--- implement/elm-time/Program.cs
+++ implement/elm-time/Program.cs
@@ -18,7 +18,7 @@ namespace ElmTime;
 
 public class Program
 {
-    public static string AppVersionId => "2024-02-11";
+    public static string AppVersionId => "2024-02-15";
 
     private static int AdminInterfaceDefaultPort => 4000;
 
