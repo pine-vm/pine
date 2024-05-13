e38b093b5f61d7d55c5a3b5c760a58e99c854136
Michael Rätzel <viir@viir.de>
2024-05-13 20:04:20 +0000

    Increase test coverage for Elm choice tag constructor usages

--- implement/test-and-train/elm-interactive-scenarios-core/elm-choice-type/context-app/src/AppModule.elm
+++ implement/test-and-train/elm-interactive-scenarios-core/elm-choice-type/context-app/src/AppModule.elm
@@ -13,6 +13,26 @@ type ChoiceTypeWithMoreArgs a b
     | Variant_Eta a b String Int ChoiceType
 
 
+construct_variant_Gamma : String -> Int -> ChoiceType
+construct_variant_Gamma a b =
+    Variant_Gamma a b
+
+
 construct_variant_Epsilon : a -> b -> String -> ChoiceTypeWithMoreArgs a b
 construct_variant_Epsilon a b c =
     Variant_Epsilon a b c
+
+
+construct_variant_Gamma_from_nested_result : Result String ( String, Result String Int ) -> Result String ChoiceType
+construct_variant_Gamma_from_nested_result outerResult =
+    case outerResult of
+        Ok ( aOk, innerResult ) ->
+            case innerResult of
+                Err err ->
+                    Err ("Inner error: " ++ err)
+
+                Ok b ->
+                    Ok (Variant_Gamma aOk b)
+
+        Err err ->
+            Err ("Outer error: " ++ err)
