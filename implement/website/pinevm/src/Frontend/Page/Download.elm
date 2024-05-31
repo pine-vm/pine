89ed1900d445dc8b1d076308447aff66be3e66d6
Michael Rätzel <viir@viir.de>
2024-05-31 13:38:49 +0000

    Update website to latest release

--- implement/website/pinevm/src/Frontend/Page/Download.elm
+++ implement/website/pinevm/src/Frontend/Page/Download.elm
@@ -48,9 +48,9 @@ dockerImageUrl =
 
 downloads : DownloadLinksByPlatform
 downloads =
-    { linux = "https://github.com/pine-vm/pine/releases/download/v0.3.6/pine-bin-v0.3.6-linux-x64.zip"
-    , windows = "https://github.com/pine-vm/pine/releases/download/v0.3.6/pine-bin-v0.3.6-win-x64.zip"
-    , macOS = "https://github.com/pine-vm/pine/releases/download/v0.3.6/pine-bin-v0.3.6-osx-x64.zip"
+    { linux = "https://github.com/pine-vm/pine/releases/download/v0.3.10/pine-bin-v0.3.10-linux-x64.zip"
+    , windows = "https://github.com/pine-vm/pine/releases/download/v0.3.10/pine-bin-v0.3.10-win-x64.zip"
+    , macOS = "https://github.com/pine-vm/pine/releases/download/v0.3.10/pine-bin-v0.3.10-osx-x64.zip"
     }
 
 
