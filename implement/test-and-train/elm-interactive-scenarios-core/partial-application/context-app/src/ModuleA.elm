37b883ad012c0a69b4dd8575c4cda00d52a592c9
Michael Rätzel <viir@viir.de>
2024-09-27 06:21:52 +0000

    Automate testing partial application across module-level declarations

--- implement/test-and-train/elm-interactive-scenarios-core/partial-application/context-app/src/ModuleA.elm
+++ implement/test-and-train/elm-interactive-scenarios-core/partial-application/context-app/src/ModuleA.elm
@@ -1,4 +1,4 @@
-module ModuleA exposing (partially_applied_a)
+module ModuleA exposing (..)
 
 
 partially_applied_a =
@@ -7,3 +7,11 @@ partially_applied_a =
 
 function_with_three_parameters param0 param1 param2 =
     [ param0, param1, param0, param2, param1 ]
+
+
+same_module_partially_applied_b =
+    partially_applied_a named_literal
+
+
+named_literal =
+    71
