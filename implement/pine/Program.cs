6a967fc3a0a4684348f3139a2caa3dbe435c12e1
Michael Rätzel <viir@viir.de>
2024-10-20 12:42:22 +0000

    Release version 0.3.22

--- implement/pine/Program.cs
+++ implement/pine/Program.cs
@@ -23,7 +23,7 @@ namespace ElmTime;
 
 public class Program
 {
-    public static string AppVersionId => "0.3.21";
+    public static string AppVersionId => "0.3.22";
 
     private static int AdminInterfaceDefaultPort => 4000;
 
