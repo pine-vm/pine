af0ea8e5745af05ccc7465a1e376c7ff7eebb5a7
Michael Rätzel <viir@viir.de>
2024-12-23 10:20:03 +0000

    update website to latest release

--- implement/website/pinevm/src/Frontend/Page/Download.elm
+++ implement/website/pinevm/src/Frontend/Page/Download.elm
@@ -55,9 +55,9 @@ vscodeMarketplaceUrl =
 
 downloads : DownloadLinksByPlatform
 downloads =
-    { linux = "https://github.com/pine-vm/pine/releases/download/v0.3.29/pine-bin-v0.3.29-linux-x64.zip"
-    , windows = "https://github.com/pine-vm/pine/releases/download/v0.3.29/pine-bin-v0.3.29-win-x64.zip"
-    , macOS = "https://github.com/pine-vm/pine/releases/download/v0.3.29/pine-bin-v0.3.29-osx-x64.zip"
+    { linux = "https://github.com/pine-vm/pine/releases/download/v0.3.30/pine-bin-v0.3.30-linux-x64.zip"
+    , windows = "https://github.com/pine-vm/pine/releases/download/v0.3.30/pine-bin-v0.3.30-win-x64.zip"
+    , macOS = "https://github.com/pine-vm/pine/releases/download/v0.3.30/pine-bin-v0.3.30-osx-x64.zip"
     }
 
 
