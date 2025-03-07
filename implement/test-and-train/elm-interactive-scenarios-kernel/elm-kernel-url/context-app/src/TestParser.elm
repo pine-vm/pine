107fa71ff954ce83d35d47a2f571e9cd3ecd485f
Michael Rätzel <viir@viir.de>
2025-03-07 07:33:24 +0000

    expand test coverage for elm/url `Url.Parser.parse`

---
+++ implement/test-and-train/elm-interactive-scenarios-kernel/elm-kernel-url/context-app/src/TestParser.elm
@@ -0,0 +1,20 @@
+module TestParser exposing( .. )
+
+
+import Url
+import Url.Parser exposing ((</>))
+
+
+
+type Route
+    = EntryRoute (Maybe Int)
+
+
+routeFromUrl : Url.Url -> Maybe Route
+routeFromUrl =
+    Url.Parser.parse
+        (Url.Parser.oneOf
+            [ Url.Parser.map (Just >> EntryRoute) (Url.Parser.s "api" </> Url.Parser.s "entry" </> Url.Parser.int)
+            , Url.Parser.map (EntryRoute Nothing) (Url.Parser.s "api" </> Url.Parser.s "entry")
+            ]
+        )
