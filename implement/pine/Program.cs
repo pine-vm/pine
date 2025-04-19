5abf20d3c86dfc282b83fa9642731b9ddc24bae1
Michael Rätzel <viir@viir.de>
2025-04-19 19:40:20 +0000

    apply recommended patches from upstream projects

--- implement/pine/Program.cs
+++ implement/pine/Program.cs
@@ -23,7 +23,7 @@ namespace ElmTime;
 
 public class Program
 {
-    public static string AppVersionId => "0.4.3";
+    public static string AppVersionId => "0.4.4";
 
     private static int AdminInterfaceDefaultPort => 4000;
 
