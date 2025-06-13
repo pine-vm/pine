63f887bbdbfc004826a4dfa4ada6af8f84141deb
Michael Rätzel <michael@xn--michaelrtzel-ncb.com>
2025-06-13 17:23:58 +0000

    publish release - Pine version 0.4.9

--- implement/pine/Program.cs
+++ implement/pine/Program.cs
@@ -23,7 +23,7 @@ namespace ElmTime;
 
 public class Program
 {
-    public static string AppVersionId => "0.4.8";
+    public static string AppVersionId => "0.4.9";
 
     private static int AdminInterfaceDefaultPort => 4000;
 
