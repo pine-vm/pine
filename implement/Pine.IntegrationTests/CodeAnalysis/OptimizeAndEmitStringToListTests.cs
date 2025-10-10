9f0a0f5f7eb82d0b1dacf75b25dabd39016dbad0
Michael Rätzel <michael@xn--michaelrtzel-ncb.com>
2025-10-10 09:54:23 +0000

    add test to track optimizations for compiling nested 'concat'

---
+++ implement/Pine.IntegrationTests/CodeAnalysis/OptimizeAndEmitStringToListTests.cs
@@ -0,0 +1,184 @@
+using AwesomeAssertions;
+using Pine.Core;
+using Pine.Core.CodeAnalysis;
+using Pine.Core.DotNet;
+using Pine.Elm;
+using System.Text;
+using Xunit;
+
+namespace Pine.IntegrationTests.CodeAnalysis;
+
+public class OptimizeAndEmitStringToListTests
+{
+    [Fact]
+    public void Parse_and_emit_optimized_String_toList()
+    {
+        var elmJsonFile =
+            """
+            {
+                "type": "application",
+                "source-directories": [
+                    "src"
+                ],
+                "elm-version": "0.19.1",
+                "dependencies": {
+                    "direct": {
+                        "elm/core": "1.0.5"
+                    },
+                    "indirect": {
+                    }
+                },
+                "test-dependencies": {
+                    "direct": {
+                    },
+                    "indirect": {
+                    }
+                }
+            }
+            """;
+
+        var elmModuleText =
+            """
+            module Test exposing (..)
+
+
+            
+            type String
+                = String Int
+
+
+            toList : String -> List Char
+            toList (String charsBlob) =
+                toListRecursive
+                    0
+                    []
+                    charsBlob
+
+
+            toListRecursive : Int -> List Char -> Int -> List Char
+            toListRecursive offset list blob =
+                let
+                    nextChar =
+                        Pine_kernel.take
+                            [ 4
+                            , Pine_kernel.skip [ offset, blob ]
+                            ]
+                in
+                if Pine_kernel.equal [ Pine_kernel.length nextChar, 0 ] then
+                    list
+
+                else
+                    toListRecursive
+                        (Pine_kernel.int_add [ offset, 4 ])
+                        (Pine_kernel.concat [ list, [ nextChar ] ])
+                        blob
+            
+            """;
+
+        var appCodeTree =
+            BlobTreeWithStringPath.EmptyTree
+            .SetNodeAtPathSorted(
+                ["elm.json"],
+                BlobTreeWithStringPath.Blob(Encoding.UTF8.GetBytes(elmJsonFile)))
+            .SetNodeAtPathSorted(
+                ["src", "Test.elm"],
+                BlobTreeWithStringPath.Blob(Encoding.UTF8.GetBytes(elmModuleText)));
+
+        var compiledEnv =
+            ElmCompiler.CompileInteractiveEnvironment(
+                appCodeTree,
+                rootFilePaths: [["src", "Test.elm"]],
+                skipLowering: true,
+                skipFilteringForSourceDirs: false)
+            .Extract(err => throw new System.Exception(err));
+
+        var parseCache = new PineVMParseCache();
+
+        var parsedEnv =
+            ElmInteractiveEnvironment.ParseInteractiveEnvironment(compiledEnv)
+            .Extract(err => throw new System.Exception("Failed parsing interactive environment: " + err));
+
+        var staticProgram =
+            CodeAnalysisTestHelper.ParseAsStaticMonomorphicProgramAndCrashOnAnyFailure(
+                parsedEnv,
+                includeDeclaration:
+                declName =>
+                {
+                    return declName == new DeclQualifiedName(["Test"], "toList");
+                },
+                parseCache);
+
+        var wholeProgramText = StaticExpressionDisplay.RenderStaticProgram(staticProgram);
+
+        var asCSharp =
+            StaticProgramCSharp.FromStaticProgram(
+                staticProgram,
+                CodeAnalysisTestHelper.DeclarationSyntaxContext);
+
+        var moduleTest = asCSharp.ModulesClasses[new DeclQualifiedName([], "Test")];
+
+        var moduleTestCSharpText =
+            moduleTest.RenderToString();
+
+        var moduleGlobalAnonymousText =
+            asCSharp.GlobalAnonymousClass.RenderToString();
+
+        moduleTestCSharpText.Trim().Should().Be(
+            """"
+            public static class Test
+            {
+                public static PineValue toListRecursive(
+                    PineValue param_1_0,
+                    PineValue param_1_1,
+                    PineValue param_1_2)
+                {
+                    PineValue local_param_1_0 =
+                        param_1_0;
+
+                    PineValue local_param_1_1 =
+                        param_1_1;
+
+                    PineValue local_param_1_2 =
+                        param_1_2;
+
+                    while (true)
+                    {
+                        PineValue local_000 =
+                            KernelFunctionFused.SkipAndTake(takeCount: 4, skipCountValue: local_param_1_0, argument: local_param_1_2);
+
+                        if (KernelFunctionSpecialized.length_as_int(local_000) == 0)
+                        {
+                            return local_param_1_1;
+                        }
+
+                        {
+                            PineValue local_param_1_0_temp =
+                                KernelFunctionSpecialized.int_add(4, local_param_1_0);
+
+                            PineValue local_param_1_1_temp =
+                                KernelFunctionFused.ListAppendItem(prefix: local_param_1_1, itemToAppend: local_000);
+
+                            local_param_1_0 =
+                                local_param_1_0_temp;
+
+                            local_param_1_1 =
+                                local_param_1_1_temp;
+                        }
+
+                        continue;
+                    }
+                }
+            }
+
+            """".Trim());
+
+        moduleGlobalAnonymousText.Trim().Should().Be(
+            """"
+            public static class Global_Anonymous
+            {
+            }
+
+            """"
+            .Trim());
+    }
+}
