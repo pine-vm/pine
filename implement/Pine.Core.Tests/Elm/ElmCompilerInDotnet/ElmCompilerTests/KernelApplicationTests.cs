738702df3593c9e58ea85c0d5681a1685ec3eaeb
Michael Rätzel <michael@xn--michaelrtzel-ncb.com>
2025-11-28 17:36:12 +0000

    update references to new namespaces

--- implement/Pine.Core.Tests/Elm/ElmCompilerInDotnet/ElmCompilerTests/KernelApplicationTests.cs
+++ implement/Pine.Core.Tests/Elm/ElmCompilerInDotnet/ElmCompilerTests/KernelApplicationTests.cs
@@ -1,8 +1,8 @@
 using AwesomeAssertions;
 using Pine.Core.CodeAnalysis;
+using Pine.Core.CommonEncodings;
 using Pine.Core.Elm;
 using Pine.Core.Interpreter.IntermediateVM;
-using Pine.Core.PopularEncodings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
