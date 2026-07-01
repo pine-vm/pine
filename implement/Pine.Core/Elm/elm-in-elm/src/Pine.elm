bd67f422fdb29a0c79cb1222f645ceac77d0c7dd
Michael Rätzel <michael@xn--michaelrtzel-ncb.com>
2026-07-01 18:15:49 +0000

    simplify inlining rendering to hexadecimal string

--- implement/Pine.Core/Elm/elm-in-elm/src/Pine.elm
+++ implement/Pine.Core/Elm/elm-in-elm/src/Pine.elm
@@ -41,7 +41,6 @@ module Pine exposing
 
 import BigInt
 import Bitwise
-import Hex
 
 
 type Expression
@@ -1388,8 +1387,17 @@ hexadecimalStringFromBlobValue : List String -> List Int -> String
 hexadecimalStringFromBlobValue acc bytes =
     case bytes of
         byte :: rest ->
+            let
+                upperNibble =
+                    byte // 16
+            in
             hexadecimalStringFromBlobValue
-                (String.padLeft 2 '0' (Hex.toString byte) :: acc)
+                (String.fromList
+                    [ hexadecimalCharacterFromInt upperNibble
+                    , hexadecimalCharacterFromInt (byte - (upperNibble * 16))
+                    ]
+                    :: acc
+                )
                 rest
 
         _ ->
@@ -1849,3 +1857,58 @@ valueFromInt_4 =
 listValue_Empty : Value
 listValue_Empty =
     ListValue []
+
+
+hexadecimalCharacterFromInt : Int -> Char
+hexadecimalCharacterFromInt int =
+    case int of
+        0 ->
+            '0'
+
+        1 ->
+            '1'
+
+        2 ->
+            '2'
+
+        3 ->
+            '3'
+
+        4 ->
+            '4'
+
+        5 ->
+            '5'
+
+        6 ->
+            '6'
+
+        7 ->
+            '7'
+
+        8 ->
+            '8'
+
+        9 ->
+            '9'
+
+        10 ->
+            'A'
+
+        11 ->
+            'B'
+
+        12 ->
+            'C'
+
+        13 ->
+            'D'
+
+        14 ->
+            'E'
+
+        15 ->
+            'F'
+
+        _ ->
+            '#'
