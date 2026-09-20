a09d32a9bfae41a47dc1740b60c597cb7d8de804
Michael Rätzel <michael@xn--michaelrtzel-ncb.com>
2026-09-20 17:50:23 +0000

    Release Pine version 0.5.8

--- implement/pine/CLI/PineCliCommand.cs
+++ implement/pine/CLI/PineCliCommand.cs
@@ -15,7 +15,7 @@ namespace Pine.CLI;
 
 public class PineCliCommand
 {
-    public static string AppVersionId => "0.5.7";
+    public static string AppVersionId => "0.5.8";
 
     internal static int AdminInterfaceDefaultPort => 4000;
 
